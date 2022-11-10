431eb3f51f1dbfc913f5cc8b3fb0c6721655b833
Bedatty <lucasbedatty01@gmail.com>
2022-11-09 22:58:44 -0300

    Fix actions

--- main.tf
+++ main.tf
@@ -1,18 +1,29 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "3.26.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "3.0.1"
     }
   }
+  required_version = ">= 1.1.0"
+
   cloud {
     organization = "aws-tf-bedatty"
 
     workspaces {
       name = "aws-tf-actions"
     }
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "teste-tfc"
   }
 }
 
