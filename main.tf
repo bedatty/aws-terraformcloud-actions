e95b4c72265ec27561bb846ddd1e4459c66a92c3
Bedatty <lucasbedatty01@gmail.com>
2022-11-09 22:44:20 -0300

    Creating VPC

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  cloud {
+    organization = "aws-tf-bedatty"
+
+    workspaces {
+      name = "aws-tf-actions"
+    }
+  }
+}
+
 resource "aws_vpc" "create_vpc" {
   cidr_block = "10.0.0.0/16"
 
